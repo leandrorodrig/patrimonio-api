b9e68d8bd0fb6e6cb3b0ec3aa7725201770f2406
Leandro Rodrigues <leo.rodrig100@gmail.com>
2018-02-25 22:35:41 -0300

    Aula 2

---
+++ src/main/java/com/algaworks/patrimonio/model/Item.java
@@ -0,0 +1,118 @@
+package com.algaworks.patrimonio.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class Item {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty
+	private String etiqueta;
+
+	@NotEmpty
+	private String descricao;
+
+	private LocalDate dataAquisicao;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the etiqueta
+	 */
+	public String getEtiqueta() {
+		return etiqueta;
+	}
+
+	/**
+	 * @param etiqueta
+	 *            the etiqueta to set
+	 */
+	public void setEtiqueta(String etiqueta) {
+		this.etiqueta = etiqueta;
+	}
+
+	/**
+	 * @return the descricao
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+
+	/**
+	 * @param descricao
+	 *            the descricao to set
+	 */
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	/**
+	 * @return the dataAquisicao
+	 */
+	public LocalDate getDataAquisicao() {
+		return dataAquisicao;
+	}
+
+	/**
+	 * @param dataAquisicao
+	 *            the dataAquisicao to set
+	 */
+	public void setDataAquisicao(LocalDate dataAquisicao) {
+		this.dataAquisicao = dataAquisicao;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
